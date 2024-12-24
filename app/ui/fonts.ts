324798b56b46e3153e654c1d07be3a65847db9ce
Azarudeen <azarudeendanish@gmail.com>
2024-12-24 20:15:22 +0530

    font lusitana added

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,5 +1,5 @@
-import { Inter} from 'next/font/google';
+import { Inter, Lusitana} from 'next/font/google';
 
 export const inter = Inter({ subsets: ['latin'] });
 // export const oswald = Oswald({subsets: ['latin']})
-// export const lusitana = Lusitana({weight: ['400']})
+export const lusitana = Lusitana({weight: ['400', '700'], subsets: ['latin']})
