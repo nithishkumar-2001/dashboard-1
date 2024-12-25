2e26cf159a6b84852bb46f45295174073c7323bc
Azarudeen <azarudeendanish@gmail.com>
2024-12-25 10:39:43 +0530

    oswald font removed

--- app/ui/acme-logo.tsx
+++ app/ui/acme-logo.tsx
@@ -1,6 +1,6 @@
 import { GlobeAltIcon } from '@heroicons/react/24/outline';
 // import { lusitana } from '@/app/ui/fonts';
-import { inter, oswald, lusitana } from '@/app/ui/fonts';
+import { inter, lusitana } from '@/app/ui/fonts';
 
 export default function AcmeLogo() {
   return (
